0cfbf3de5191ca4c2f2d00fa8ee3231e8b2c20cd
Bacho172 <adarsim1@op.pl>
2018-03-09 09:45:20 +0100

    Usuwanie zmian (NetBeans)

--- src/clickabletest/ClickableObject.java
+++ src/clickabletest/ClickableObject.java
@@ -20,9 +20,6 @@ public abstract class ClickableObject extends GameObject implements MouseListene
     public ClickableObject(Dimension size, Point position) {
         super(size, position);
         Handler.getGraphicsEngine().addMouseListener(this);
-        // kolejna zmiana
-        // i kolejna
-        // inna zmiana
     }
 
     private boolean mouseIsAbove(MouseEvent e) {
